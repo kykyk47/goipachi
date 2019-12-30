a3aa2b8d8c1714360d8508509110ac9e86e7d79f
kykyk47 <sakoshin8mk2@outlook.jp>
2019-12-30 23:16:29 +0900

    1230

--- main.cpp
+++ main.cpp
@@ -1,53 +1,87 @@
 #include "pch.h"
 #include <iostream>
-#include <stdio.h>
+
 #include <gl/glut.h>
 #include <math.h>
-#include <IL/il.h>
-#include <IL/ilut.h>
 
+#include <objbase.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <gdiplus.h>
 
+using namespace Gdiplus;
+
+#define WIDTH 800
+#define HEIGHT 450
+
+GdiplusStartupInput gdiPSI;
+ULONG_PTR gdiPT;
+GLuint tex;
+
+void LoadImagePNG(const wchar_t* filename, GLuint &texture)
+{
+	glEnable(GL_TEXTURE_2D);
+	Bitmap bmp(filename);
+	BitmapData data;
+	bmp.LockBits(0, ImageLockModeRead, PixelFormat32bppARGB, &data);
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexImage2D(GL_TEXTURE_2D, 0, 4, data.Width, data.Height, 0, GL_BGRA_EXT, GL_UNSIGNED_BYTE, data.Scan0);
+	glBindTexture(GL_TEXTURE_2D, 0);
+	bmp.UnlockBits(&data);
+}
+
+void end()
+{
+	GdiplusShutdown(gdiPT);
+}
 void display(void)
 {
-	glClear(GL_COLOR_BUFFER_BIT);
-	glFlush();
-}
-
-void resize(int w, int h)
-{
-	/* ウィンドウ全体をビューポートにする */
-	glViewport(0, 0, w, h);
-
-	/* 変換行列の初期化 */
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
+	glOrtho(0.0, WIDTH, HEIGHT, 0.0, -1.0, 1.0);
 
-	/* スクリーン上の表示領域をビューポートの大きさに比例させる */
-	glOrtho(-w / 200.0, w / 200.0, -h / 200.0, h / 200.0, -1.0, 1.0);
+	glBindTexture(GL_TEXTURE_2D, tex);
+	glEnable(GL_TEXTURE_2D);
+	glEnable(GL_ALPHA_TEST);
+	glBegin(GL_POLYGON);
+	glTexCoord2f(0.0f, 1.0f); glVertex2d(10, 230);//左下
+	glTexCoord2f(0.0f, 0.0f); glVertex2d(10, 10);//左上
+	glTexCoord2f(1.0f, 0.0f); glVertex2d(310, 10);//右上
+	glTexCoord2f(1.0f, 1.0f); glVertex2d(310, 230);//右下
+	glEnd();
+	glDisable(GL_ALPHA_TEST);
+	glDisable(GL_TEXTURE_2D);
+
+	glutSwapBuffers();
 }
 
-void init(void)
+void idle(void)
 {
-	glClearColor(0.0, 0.0, 0.0, 1.0);
-	glEnable(GL_TEXTURE_2D); // 2Dテクスチャを有効にする
+	glutPostRedisplay();
+}
 
+void Init() {
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glOrtho(0, WIDTH, HEIGHT, 0, -1, 1);
+	GdiplusStartup(&gdiPT, &gdiPSI, NULL);
+	LoadImagePNG(L"start.png", tex);
 }
 
 int main(int argc, char *argv[])
 {
-	ilInit();
-	ilutRenderer(ILUT_OPENGL);
-	wchar_t unko[24] = L"start.png";
-	
-	GLuint texture = ilutGLLoadImage(unko);
-
-	glutInitWindowPosition(200, 100);
-	glutInitWindowSize(800, 450);
+	atexit(end);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(WIDTH, HEIGHT);
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
-	glutCreateWindow(argv[0]);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
+	glutCreateWindow("画像を読み込んで表示");
 	glutDisplayFunc(display);
-	glutReshapeFunc(resize);
-	init();
+	glutIdleFunc(idle);
+	Init();
 	glutMainLoop();
 	return 0;
 }

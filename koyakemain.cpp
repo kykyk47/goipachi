3640ca47dee63982aa1f9ad4bbb1356207e1ec21
kykyk47 <sakoshin8mk2@outlook.jp>
2020-01-03 21:27:44 +0900

    0103_3

--- koyakemain.cpp
+++ koyakemain.cpp
@@ -22,8 +22,8 @@
 
 using namespace Gdiplus;
 
-#define WIDTH 800
-#define HEIGHT 450
+#define WIDTH 1280
+#define HEIGHT 720
 
 GdiplusStartupInput gdiPSI;
 ULONG_PTR gdiPT;
@@ -32,8 +32,12 @@ GLuint tex_player2;
 GLuint tex_player3;
 bool onMoveKeyPress_L = false;
 bool onMoveKeyPress_R = false;
-bool jump = false;
+bool player_jump = false;
+bool player_walk = false;
 int jump_timer = 0;  //キャラクターがジャンプしてからの時間を計測
+int walk_timer = 0;
+int walk_timer100 = 0;
+
 
 struct Position
 {
@@ -70,16 +74,47 @@ void display(void)
 	glLoadIdentity();
 	glOrtho(0.0, WIDTH, HEIGHT, 0.0, -1.0, 1.0);
 
-	glBindTexture(GL_TEXTURE_2D, tex_player1);
+	if (walk_timer == 0)
+	{
+		glBindTexture(GL_TEXTURE_2D, tex_player2);
+	}
+	else
+	{
+		switch (walk_timer)
+		{
+		case 1:  glBindTexture(GL_TEXTURE_2D, tex_player3); break;
+		case 2:  glBindTexture(GL_TEXTURE_2D, tex_player2); break;
+		case 3:  glBindTexture(GL_TEXTURE_2D, tex_player1); break;
+		case 0:  glBindTexture(GL_TEXTURE_2D, tex_player2); break;
+		}
+	}
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	glEnable(GL_TEXTURE_2D);
 	glEnable(GL_ALPHA_TEST);
 	glBegin(GL_POLYGON);
-	glTexCoord2f(0.0f, 1.0f); glVertex2d(0 + player.x, 448 + player.y);//左下
-	glTexCoord2f(0.0f, 0.0f); glVertex2d(0 + player.x, 416 + player.y);//左上
-	glTexCoord2f(1.0f, 0.0f); glVertex2d(32 + player.x, 416 + player.y);//右上
-	glTexCoord2f(1.0f, 1.0f); glVertex2d(32 + player.x, 448 + player.y);//右下
+	switch (player.direction)
+	{
+	case 0:
+	{
+		glTexCoord2f(0.0f, 1.0f); glVertex2d(32 + player.x, 448 + player.y);//左下
+		glTexCoord2f(0.0f, 0.0f); glVertex2d(32 + player.x, 416 + player.y);//左上
+		glTexCoord2f(1.0f, 0.0f); glVertex2d(0 + player.x, 416 + player.y);//右上
+		glTexCoord2f(1.0f, 1.0f); glVertex2d(0 + player.x, 448 + player.y);//右下
+		break;
+	}
+
+	case 1:
+	{
+		glTexCoord2f(0.0f, 1.0f); glVertex2d(0 + player.x, 448 + player.y);//左下
+		glTexCoord2f(0.0f, 0.0f); glVertex2d(0 + player.x, 416 + player.y);//左上
+		glTexCoord2f(1.0f, 0.0f); glVertex2d(32 + player.x, 416 + player.y);//右上
+		glTexCoord2f(1.0f, 1.0f); glVertex2d(32 + player.x, 448 + player.y);//右下
+		break;
+	}
+	}
+
+
 	glEnd();
 	glDisable(GL_ALPHA_TEST);
 	glDisable(GL_TEXTURE_2D);
@@ -92,29 +127,37 @@ void idle(void)
 {
 	if (onMoveKeyPress_L == true)
 	{
-		player.x -= 0.5;
+		player.x -= 0.125;
 	}
 
 	if (onMoveKeyPress_R == true)
 	{
-		player.x += 0.5;
+		player.x += 0.125;
 	}
 
-	if (jump == true)
+	if (player_jump == true)
 	{
-		player.y = player.y - (12 * jump_timer - 9.8*jump_timer*jump_timer *0.5)*0.12;
+		if (jump_timer < 10)
+		{
+			player.y = player.y - (15.0 * jump_timer - 9.68* jump_timer * sqrt(jump_timer) *0.5)*0.02;//ジャンプのときのプレイヤーの動き
+		}
+
+		else if (jump_timer >= 10)
+		{
+			player.y = player.y - (15.0 * jump_timer - 9.68* jump_timer * sqrt(jump_timer) *0.5)*0.0015;//ジャンプのときのプレイヤーの動き
+		}
 
 		if (player.y > 0) //ブロックの上に着地する場合，その座標とする
 		{
 			player.y = 0;
 			jump_timer = 0;
-			jump = false;
+			player_jump = false;
 		}
 
 	}
 
 
-	printf("%.2f,%.2f %d\n", player.x, player.y, jump_timer);
+	printf("%.2f,%.2f %d %d \n", player.x, player.y, jump_timer, walk_timer100);
 	glutPostRedisplay();
 }
 
@@ -124,9 +167,9 @@ void keyboard(unsigned char key, int x, int y)
 	case 'q':
 	case '\033':  /* '\033' は ESC の ASCII コード */
 		exit(0); break;
-	case 'a': onMoveKeyPress_L = true; player.direction = 0; break;
-	case 'd': onMoveKeyPress_R = true; player.direction = 1; break;
-	case '\040': if (jump == false) { jump = true; } break;
+	case 'a': onMoveKeyPress_L = true; /*MoveLock_R = true;*/ player.direction = 0; break;
+	case 'd': onMoveKeyPress_R = true; /*MoveLock_L = true;*/ player.direction = 1; break;
+	case '\040': if (player_jump == false) { player_jump = true; } break;
 	default:
 		break;
 	}
@@ -156,15 +199,36 @@ void Init() {
 }
 
 void timer(int value) {
-	glutPostRedisplay();
 
-	if (jump == true)
+	glutPostRedisplay();
+	glutTimerFunc(15, timer, 0);
+
+	if (player_jump == true)
 	{
 		jump_timer++;
 	}
 
-	glutTimerFunc(100, timer, 0);
+	if (onMoveKeyPress_L == true || onMoveKeyPress_R == true) //歩きアニメーションのため （画像１→２→３→２というふうに歩き中には４枚の画像を連続で表示する）
+	{
+		if (walk_timer100 == 300)
+		{
+			walk_timer100 = 0;
+		}
+
+		else
+		{
+			walk_timer100 += 100;
+		}
+	}
+
+	else
+	{
+		walk_timer100 = 0;
+	}
+
+	walk_timer = walk_timer100 / 100;
 }
+
 
 int main(int argc, char *argv[])
 {
@@ -173,9 +237,9 @@ int main(int argc, char *argv[])
 	glutInitWindowSize(WIDTH, HEIGHT);
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
-	glutCreateWindow("画像を読み込んで表示");
+	glutCreateWindow("goipachi ver.0.0.0");
 	glutDisplayFunc(display);
-	glutTimerFunc(100, timer, 0);
+	glutTimerFunc(15, timer, 0);
 	glutKeyboardFunc(keyboard);
 	glutKeyboardUpFunc(keyboardUp);
 	glutIdleFunc(idle);

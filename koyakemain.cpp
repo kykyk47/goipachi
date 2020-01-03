1c43910b6fbfce6e37996dfdf217ad5523f2cb71
kykyk47 <sakoshin8mk2@outlook.jp>
2020-01-03 14:52:40 +0900

    0103

---
+++ koyakemain.cpp
@@ -0,0 +1,100 @@
+//小宅用
+
+#include "pch.h"
+#include <iostream>
+#include <gl/glut.h>
+#include <math.h>
+#include <objbase.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <gdiplus.h>
+
+
+//松本用
+/*
+#include <objbase.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <gdiplus.h>
+#include <gl/glut.h>
+*/
+
+using namespace Gdiplus;
+
+#define WIDTH 800
+#define HEIGHT 450
+
+GdiplusStartupInput gdiPSI;
+ULONG_PTR gdiPT;
+GLuint tex;
+
+void LoadImagePNG(const wchar_t* filename, GLuint &texture)
+{
+	glEnable(GL_TEXTURE_2D);
+	Bitmap bmp(filename);
+	BitmapData data;
+	bmp.LockBits(0, ImageLockModeRead, PixelFormat32bppARGB, &data);
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexImage2D(GL_TEXTURE_2D, 0, 4, data.Width, data.Height, 0, GL_BGRA_EXT, GL_UNSIGNED_BYTE, data.Scan0);
+	glBindTexture(GL_TEXTURE_2D, 0);
+	bmp.UnlockBits(&data);
+}
+
+void end()
+{
+	GdiplusShutdown(gdiPT);
+}
+void display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glOrtho(0.0, WIDTH, HEIGHT, 0.0, -1.0, 1.0);
+
+	glBindTexture(GL_TEXTURE_2D, tex);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glEnable(GL_TEXTURE_2D);
+	glEnable(GL_ALPHA_TEST);
+	glBegin(GL_POLYGON);
+	glTexCoord2f(0.0f, 1.0f); glVertex2d(10, 230);//左下
+	glTexCoord2f(0.0f, 0.0f); glVertex2d(10, 10);//左上
+	glTexCoord2f(1.0f, 0.0f); glVertex2d(310, 10);//右上
+	glTexCoord2f(1.0f, 1.0f); glVertex2d(310, 230);//右下
+	glEnd();
+	glDisable(GL_ALPHA_TEST);
+	glDisable(GL_TEXTURE_2D);
+	glDisable(GL_BLEND);
+
+	glutSwapBuffers();
+}
+
+void idle(void)
+{
+	glutPostRedisplay();
+}
+
+void Init() {
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glOrtho(0, WIDTH, HEIGHT, 0, -1, 1);
+	GdiplusStartup(&gdiPT, &gdiPSI, NULL);
+	LoadImagePNG(L"walk1.png", tex);
+}
+
+int main(int argc, char *argv[])
+{
+	atexit(end);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(WIDTH, HEIGHT);
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
+	glutCreateWindow("画像を読み込んで表示");
+	glutDisplayFunc(display);
+	glutIdleFunc(idle);
+	Init();
+	glutMainLoop();
+	return 0;
+}
